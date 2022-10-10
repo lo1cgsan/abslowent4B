cc516765e0e911be623b0c81a8b06f9886eb019f
lo1cgsan <lo1cgsan@gmail.com>
2022-10-10 09:40:50 +0200

    dodanie szablonu bazowego (coś jeszcze nie działa!)

--- osoby/views.py
+++ osoby/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Witaj w Django!</h1>")
+
+def lista(request):
+    return HttpResponse("<h1>Lista absolwentów!</h1>")
