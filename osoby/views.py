2392dae41ad6f54008b5e248eb30d959587ded9e
lo1cgsan <lo1cgsan@gmail.com>
2022-10-11 11:41:09 +0200

    lista absolwentów w oparciu o szablon bazowy

--- osoby/views.py
+++ osoby/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from osoby.models import Klasa, Absolwent
 
 def index(request):
     return HttpResponse("<h1>Witaj w Django!</h1>")
 
 def lista(request):
-    return HttpResponse("<h1>Lista absolwentów!</h1>")
+    absolwenci = Absolwent.objects.all()
+    kontekst = {'absolwenci': absolwenci}
+    return render(request, 'osoby/lista_absolwentow.html', kontekst)
