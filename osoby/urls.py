69fdca6d8c0dfed79325706d211c8c200a936a42
lo1cgsan <lo1cgsan@gmail.com>
2022-10-25 11:41:09 +0200

    autentykacja, przycisk dodawania wiadomości

--- osoby/urls.py
+++ osoby/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('lista/', views.lista, name='lista'),
     path('loguj/', views.loguj_osobe, name='loguj-osobe'),
+    path('wyloguj/', views.wyloguj_osobe, name='wyloguj-osobe'),
 ]
--- osoby/views.py
+++ osoby/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from osoby.models import Klasa, Absolwent
 from osoby.forms import UserLoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
 
 def index(request):
     return HttpResponse("<h1>Witaj w Django!</h1>")
@@ -16,8 +18,22 @@ def loguj_osobe(request):
     if request.method == 'POST':
         form = UserLoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            nazwa = form.cleaned_data['nazwa']
+            haslo = form.cleaned_data['haslo']
+            user = authenticate(request, username=nazwa, password=haslo)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Zostałeś zalogowany!")
+                return redirect(reverse('osoby:lista'))
+            else:
+                messages.error(request, "Błędny login lub hasło!")
     else:
         form = UserLoginForm()
     kontekst = {'form': form}
     return render(request, 'osoby/loguj_osobe.html', kontekst)
+
+
+def wyloguj_osobe(request):
+    logout(request)
+    messages.info(request, "Zostałeś wylogowany!")
+    return redirect(reverse('osoby:lista'))
